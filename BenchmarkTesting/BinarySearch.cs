e6ff05d2d9c3091e809fc6d8361c558c042fbbf7
Alston Callwood <alston80@gmail.com>
2021-08-16 18:40:57 -0400

    Add project files.

---
+++ BenchmarkTesting/BinarySearch.cs
@@ -0,0 +1,40 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkTesting
+{
+    public class BinarySearch
+    {
+        List<string> dinosaurs = new List<string>();
+
+        public BinarySearch()
+        {
+            dinosaurs.Add("Pachycephalosaurus");
+            dinosaurs.Add("Amargasaurus");
+            dinosaurs.Add("Mamenchisaurus");
+            dinosaurs.Add("Deinonychus");
+        }
+
+        [Benchmark]
+        public void BinaryListSearch()
+        {                        
+            var result = dinosaurs.BinarySearch("Deinonychus");
+        }
+
+        [Benchmark]
+        public void ListSearch()
+        {
+            var result = dinosaurs.Where(a => a == "Deinonychus").FirstOrDefault();
+        }
+
+        [Benchmark]
+        public void ListIndexOfSearch()
+        {
+            var result = dinosaurs.IndexOf("Deinonychus");
+        }
+    }
+}
---
+++ BenchmarkTesting/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+
+namespace BenchmarkTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BenchmarkRunner.Run<BinarySearch>();
+        }
+    }
+}
